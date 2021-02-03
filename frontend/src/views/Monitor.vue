cdc0751c46e8be6b4d48bf8e11dd2096f477c3bf
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-02-04 00:34:36 +0300

    Implement basic monitor for ugur

---
+++ frontend/src/views/Monitor.vue
@@ -0,0 +1,77 @@
+<template lang="pug">
+  .display-frame(v-if="pinnedQuestion")
+    img.quote-left(src="@/assets/quote-left-solid.svg" alt="quote-left")
+    .question
+      p.question-text {{ pinnedQuestion.text }}
+      h2.question-author — {{ pinnedQuestion.author }}
+    .votes
+      a-icon(type="like" theme="filled")
+      span {{ pinnedQuestion.votes }}
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState('event', ['event']),
+    pinnedQuestion() {
+      return this.event.questions?.find(question => question.isPinned)
+    }
+  },
+  methods: {
+    ...mapActions('event', ['joinEvent', 'setEventId'])
+  },
+  async created() {
+    this.setEventId(this.$route.params.eventId)
+
+    await this.joinEvent()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.display-frame {
+  padding: 20px 30px;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+  width: 950px;
+  display: flex;
+
+  .question {
+    flex: 10;
+
+    .question-text {
+      font-family: sans-serif;
+      font-size: 1.25rem;
+      color: aliceblue;
+    }
+
+    .question-author {
+      margin: 0;
+      margin-bottom: 0.5rem;
+      font-size: 1.1rem;
+      font-family: serif;
+      font-style: italic;
+      color: aliceblue;
+    }
+  }
+
+  .quote-left {
+    height: 60px;
+    margin-top: 10px;
+    margin-right: 2em;
+  }
+
+  .votes {
+    color: aliceblue;
+    flex: 1;
+    font-size: 1.2rem;
+    text-align: right;
+
+    i {
+      margin-right: 0.5em;
+    }
+  }
+}
+</style>
